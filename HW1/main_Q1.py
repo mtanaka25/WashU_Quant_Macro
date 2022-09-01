ee55f108e706a1b75ccecdc58b773a36638bbd36
TanaPanda <masaki.tanaka@tnpnd.com>
2022-09-01 10:54:56 -0500

    initial commit

---
+++ HW1/main_Q1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  1 10:51:54 2022
+
+@author: tanapanda
+"""
+
---
+++ HW1/mtQuantMacroHW1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  1 10:51:54 2022
+
+@author: tanapanda
+"""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
