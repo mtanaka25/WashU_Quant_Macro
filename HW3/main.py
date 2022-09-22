80d25597060649718e6ad05c1e1397930e4f41d6
TanaPanda <masaki.tanaka@tnpnd.com>
2022-09-22 15:15:01 -0500

    done ecept for the last question

--- HW3/main.py
+++ HW3/main.py
@@ -13,20 +13,58 @@ Requirement:
 Create Sep 20, 2022 (Masaki Tanaka, Washington University in St. Louis)
 
 """
+# =========================================================================
+# Settings
+# =========================================================================
+seed_to_use = 1234
 
-from mtQuantMacro3 import SIMModel
+SCF2019_file = 'rscfp2019.dta'
+SCF2004_file = 'rscfp2004.dta'
+
+
+# =========================================================================
+# Load packages
+# =========================================================================
+from mtQuantMacro3 import SIMModel, SCF_hetero_income
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
 
+
+
+
+# =========================================================================
+# Question 1.
+# =========================================================================
+print("\n",
+      "\n **********************************",\
+      "\n           Question 1.             ",\
+      "\n **********************************",\
+      )
+# -------------------------------------------------------------------------
+# 1) Question 1 (a)-(e)
+# -------------------------------------------------------------------------
+# Create an instance
 model_T = SIMModel()
+# Discretize a la Tauchen
 model_T.discretize(method='Tauchen')
-model_T.run_simulation(fname_header='Tauchen')
+# Run simulation
+model_T.run_simulation(fname_header='Tauchen', fixed_seed=seed_to_use)
 
+# -------------------------------------------------------------------------
+# 2) Question 1 (f)-(g)
+# -------------------------------------------------------------------------
+# Create another instance
 model_R = SIMModel()
+# Discretize a la Rouwehhorst
 model_R.discretize(method='Rouwenhorst')
-model_R.run_simulation(fname_header='Rouwenhorst')
+# Run simulation (To compare with Tauchen, fix the seed)
+model_R.run_simulation(fname_header='Rouwenhorst', fixed_seed=seed_to_use)
 
+# -------------------------------------------------------------------------
+# 3) Plot the results of (g)  
+# -------------------------------------------------------------------------
+# Gini coeficients by ages in each model
 fig1 = plt.figure(figsize=(8, 6))
 plt.plot(model_T.age_list, model_T.Gini_by_age, 
              color='blue', linestyle='dashed',
@@ -35,6 +73,7 @@ plt.plot(model_R.age_list, model_R.Gini_by_age,
              color='red', label='Rouwehhorst')
 fig1.savefig('Comparison_Gini.png', dpi=300)
 
+# Lorenz curve for age 65 in each models
 fig2 = plt.figure(figsize=(8, 6))
 plt.plot(model_T.Lorenz_by_age[-1][0], 
          model_T.Lorenz_by_age[-1][0], 
@@ -52,4 +91,35 @@ plt.vlines(1, 0, 1, color='black', linewidth = 0.5,
         linestyles='--')
 plt.legend(frameon = False)
 fig2.savefig('Comparison_Lorenz_Y65.png', dpi=300)
-print('end')
+
+
+# =========================================================================
+# Question 2.
+# =========================================================================
+
+SCF2019 = SCF_hetero_income(fname=SCF2019_file)
+SCF2019.calc_Lorenz_for_three_groups(fname_header='SCF2019')
+SCF2019.calc_Gini_index_by_age(fname_header='SCF2019')
+
+SCF2004 = SCF_hetero_income(fname=SCF2004_file)
+SCF2004.calc_Gini_index_by_age(fname_header='SCF2004')
+
+fig3 = plt.figure(figsize=(8, 6))
+plt.plot(model_T.age_list, model_T.Gini_by_age, 
+             color='blue', linestyle='dashed',
+             label='Tauchen')
+plt.plot(model_R.age_list, model_R.Gini_by_age, 
+             color='purple', label='Rouwehhorst')
+plt.plot(model_R.age_list, SCF2019.Gini_by_age, 
+             color='red', label='SCF 2019')
+fig3.savefig('Comparison_Gini_SCF2019.png', dpi=300)
+
+fig4 = plt.figure(figsize=(8, 6))
+plt.plot(model_T.age_list, model_T.Gini_by_age, 
+             color='blue', linestyle='dashed',
+             label='Tauchen')
+plt.plot(model_R.age_list, model_R.Gini_by_age, 
+             color='purple', label='Rouwehhorst')
+plt.plot(model_R.age_list, SCF2004.Gini_by_age, 
+             color='red', label='SCF 2019')
+fig4.savefig('Comparison_Gini_SCF2004.png', dpi=300)
--- HW3/mtQuantMacro3.py
+++ HW3/mtQuantMacro3.py
@@ -10,19 +10,35 @@ at Washington University in St. Louis.
 Create Sep 20, 2022 (Masaki Tanaka, Washington University in St. Louis)
 
 """
+from http.cookies import SimpleCookie
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
 import numpy as np
-import pandas as pd
 from scipy.stats import norm, lognorm
 from copy import deepcopy
 from tabulate import tabulate
-from random import randrange, seed
+
+import pandas as pd
+from statsmodels.tsa.filters.hp_filter import hpfilter
+from statsmodels.tsa.ar_model import AutoReg, AutoRegResults
 
 
+class SIMModel_super:
+    def __init__(self):
+        pass
+    
+    def discrete_Gini_index(self, Lorenz_curve):
+        Gini_contrib = [(Lorenz_curve[1, i-1] + Lorenz_curve[1, i])*(Lorenz_curve[0, i] - Lorenz_curve[0, i-1])*0.5
+                        for i in range(1, np.size(Lorenz_curve, 1))]
+        Gini_contrib.insert(0, Lorenz_curve[1, 1] *Lorenz_curve[0, 1] * 0.5)
+        
+        Gini_index = 0.5 -  np.sum(Gini_contrib)
+        
+        return Gini_index
+        
 
-class SIMModel:
+class SIMModel(SIMModel_super):
     def __init__(self,
                  rho       = 0.9900,
                  var_eps   = 0.0425,
@@ -65,7 +81,7 @@ class SIMModel:
         y_N     = self.Omega * self.sig_y20
         y_grid  = np.linspace(-y_N, y_N, self.n_grids)
         Y_grid  = np.exp(y_grid)
-
+        
         # Calculate the step size
         h = (2 * y_N)/(self.n_grids-1)
         
@@ -73,14 +89,14 @@ class SIMModel:
         trans_mat = [ 
             [self.tauchen_trans_mat_ij(i, j, y_grid, h) for j in range(self.n_grids)]
             for i in range(self.n_grids)]
-
+        
         self.Y_grid = Y_grid
         self.y_grid = y_grid
-        self.trans_mat = trans_mat
+        self.trans_mat = np.array(trans_mat)
         self.step_size = h
-
-        return y_grid, trans_mat
         
+        return y_grid, trans_mat
+    
     
     def tauchen_trans_mat_ij(self, i, j, y_grid, h):
         if j == 0:
@@ -126,12 +142,16 @@ class SIMModel:
                     + pi * Pi_N3
                     + (1 - pi) * Pi_N4
             )
+            
+            # Divide all but the top and bottom rows by two so that the 
+            # elements in each row sum to one (Kopecky & Suen[2010, RED]).
+            Pi_N[1:-2, :] *= 0.5
         
         self.Y_grid = Y_grid
         self.y_grid = y_grid
         self.trans_mat = Pi_N
         self.step_size = h
-
+        
         return y_grid, Pi_N
     
     
@@ -149,7 +169,7 @@ class SIMModel:
         fig0 = plt.figure(figsize=(8, 6))
         plt.hist(Y20_samples, bins=25, density=True)
         fig0.savefig(fname_header+'_Sample_histgram.png', dpi=300)
-
+        
         
         # -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
         # Derive and plot Lorenz curve for earnings itself (not log earning)
@@ -159,10 +179,10 @@ class SIMModel:
         cum_N_share =  np.linspace(0, 1, self.n_samples)
         
         Lorenz_Y20_original = np.array([cum_N_share, cum_Y20_share])
-                        
+        
         self.Lorenz_Y20_original = Lorenz_Y20_original
         
-
+        
         # Plot the Lorenz curve        
         fig1 = plt.figure(figsize=(8, 6))
         plt.plot(Lorenz_Y20_original[0], Lorenz_Y20_original[0], 
@@ -175,7 +195,7 @@ class SIMModel:
                    linestyles='--')
         plt.legend(frameon = False)
         fig1.savefig(fname_header+'_Lorenz_Y20_original.png', dpi=300)
-
+        
         # -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
         # Calculate the probability of each grid point
         # -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
@@ -191,9 +211,9 @@ class SIMModel:
         
         fig2 = plt.figure(figsize=(8, 6))
         plt.bar(self.Y_grid, m20)
-
+        
         fig2.savefig(fname_header+'_PDF_Y20.png', dpi=300)
-
+        
         self.m20 = m20
         
         # -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
@@ -214,21 +234,20 @@ class SIMModel:
                    linestyles='--')
         plt.legend(frameon = False)
         fig3.savefig(fname_header+'_Lorenz_Y20.png', dpi=300)
-
-
+        
+        
         # -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
         # Compute the Lorenz curve based on grid points 
         # -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+       
         n_ages = len(self.age_list)
         m_mat = np.empty((n_ages, self.n_grids))
         
-        trans_mat = np.array(self.trans_mat)
         m_mat[0, :] = m20
         
         m_a = np.array(m20).reshape(1, -1)
         
         for a in range(n_ages-1):
-            m_a_1 = m_a @ trans_mat
+            m_a_1 = m_a @ self.trans_mat
             m_mat[a+1, :] = m_a_1.flatten()
             m_a = deepcopy(m_a_1)
         self.m_mat = m_mat
@@ -282,28 +301,106 @@ class SIMModel:
         dist_filtered = distribution[distribution > 0]
         Y_grid_filterd = np.array(self.Y_grid)[distribution > 0]
         
-        # Calculate the cumulative share in terms of earnings
+        # Calculate the cumulative share in aggregate earnings
         Y_contrib_filtered = dist_filtered * Y_grid_filterd
         cum_Y_share = np.cumsum(Y_contrib_filtered)/np.sum(Y_contrib_filtered)
         cum_Y_share = np.insert(cum_Y_share, 0 , 0.0)
         
-        # Calculate the cumulative share in terms of samples
+        # Calculate the cumulative share in total samples
         cum_N_share = np.cumsum(dist_filtered)/np.sum(dist_filtered)
         cum_N_share = np.insert(cum_N_share, 0 , 0.0)
         
         Lorenz_curve = np.array([cum_N_share, cum_Y_share])
+        return Lorenz_curve
+
+
+class SCF_hetero_income(SIMModel_super):
+    def __init__(self, fname):
+        # Load SCF data
+        df = pd.read_stata(fname)
         
+        # Pick up the necessary items
+        # -- 'wgt': weight, 'age': age, 'wageinc': wage income 
+        df = df.filter(['wgt', 'age', 'wageinc']) 
+        
+        # Wage income for some samples is recorded as zero.
+        # Exclude such samples.
+        df = df.replace(0, np.nan)
+        df = df.dropna()
+        
+        # Exclude samples whose age is not in [20, 65]
+        df = df[(df.age>19)&(df.age<66)]
+        
+        df = df.sort_values('wageinc')
+        self.df = df
+    
+    def empirical_Lorenz_curve(self, Y_vec, weight_vec):
+        if type(Y_vec) is list:
+            Y_vec = np.array(Y_vec)
+        elif type(Y_vec) is pd.core.series.Series:
+            Y_vec = Y_vec.to_numpy()
+        
+        if type(weight_vec) is list:
+            weight_vec = np.array(weight_vec)
+        elif type(weight_vec) is pd.core.series.Series:
+            weight_vec = weight_vec.to_numpy()
+        
+        # Calculate the cumulative share in aggregate earnings
+        Y_contrib = Y_vec * weight_vec
+        cum_Y_share = np.cumsum(Y_contrib)/np.sum(Y_contrib)
+        cum_Y_share = np.insert(cum_Y_share, 0 , 0.0)
+        
+        # Calculate the cumulative share in total samples
+        cum_N_share = np.cumsum(weight_vec)/np.sum(weight_vec)
+        cum_N_share = np.insert(cum_N_share, 0 , 0.0)
+        
+        Lorenz_curve = np.array([cum_N_share, cum_Y_share])
         return Lorenz_curve
     
-    def discrete_Gini_index(self, Lorenz_curve):
-        Gini_contrib = [(Lorenz_curve[1, i-1] + Lorenz_curve[1, i])*(Lorenz_curve[0, i] - Lorenz_curve[0, i-1])*0.5
-                        for i in range(1, np.size(Lorenz_curve, 1))]
-        Gini_contrib.insert(0, Lorenz_curve[1, 1] *Lorenz_curve[0, 1] * 0.5)
+    def calc_Lorenz_for_three_groups(self, fname_header='SCF'):
         
-        Gini_index = 0.5 -  np.sum(Gini_contrib)
+        Lorenz_Y20_25 = self.empirical_Lorenz_curve(
+            (self.df[self.df.age<26]).wageinc,
+            (self.df[self.df.age<26]).wgt
+            )
+        Lorenz_Y40_45 = self.empirical_Lorenz_curve(
+            (self.df[(self.df.age>39)&(self.df.age<46)]).wageinc,
+            (self.df[(self.df.age>39)&(self.df.age<46)]).wgt
+            )
         
-        return Gini_index
+        Lorenz_Y60_65 = self.empirical_Lorenz_curve(
+            (self.df[self.df.age>59]).wageinc,
+            (self.df[self.df.age>59]).wgt
+            )
+        
+        fig0 = plt.figure(figsize=(8, 6))
+        plt.plot(np.linspace(0,1,5), np.linspace(0,1,5),
+                 '-', linewidth = 0.5, color = 'green', label='perfect equality')
+        plt.plot(Lorenz_Y20_25[0], Lorenz_Y20_25[1],
+                 color='blue', ls='dashed', label='Ages 20-25')
+        plt.plot(Lorenz_Y40_45[0], Lorenz_Y40_45[1], 'purple', label='ages 40-45')
+        plt.plot(Lorenz_Y60_65[0], Lorenz_Y60_65[1],
+                 color='red', lw=3, label='Ages 60-65')
+        plt.hlines(0, 0, 1, color='black', linewidth = 0.5, 
+                   linestyles='--', label='perfect inequality')
+        plt.vlines(1, 0, 1, color='black', linewidth = 0.5, 
+                   linestyles='--')
+        plt.legend(frameon = False)
+        fig0.savefig(fname_header+'_Lorenz_by_group.png', dpi=300)        
         
         
-
-    
+    def calc_Gini_index_by_age(self, fname_header='SCF'):
+        
+        Lorenz_by_age = [self.empirical_Lorenz_curve(
+                        (self.df[self.df.age==age]).wageinc,
+                        (self.df[self.df.age==age]).wgt
+                        )
+                         for age in range(20, 66, 1)]
+        self.Lorenz_by_age = Lorenz_by_age
+        Gini_by_age =[self.discrete_Gini_index(Lorenz_by_age[i])
+                      for i in range(len(Lorenz_by_age))]
+        self.Gini_by_age = Gini_by_age
+        
+        fig = plt.figure(figsize = (8, 6))
+        plt.plot(range(20, 66, 1), Gini_by_age, color='red', lw = 3)
+        fig.savefig(fname_header+'_Gini_coefficients_by_age.png', dpi=300)     
